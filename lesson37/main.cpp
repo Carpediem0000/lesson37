d897c7db8d9ed0789ed48f8c78dee275e9158a7f
Vanya Zinovchuk(Carpediem0000) <zinovchukdfuz2@gmail.com>
2023-11-24 19:36:49 +0200

    fg

--- lesson37/main.cpp
+++ lesson37/main.cpp
@@ -1,31 +1,58 @@
 #include "MyStack.h"
+#include <string>
+
+bool VaildBrackets(std::string line) {
+	myStack::Stack<char> b;
+	for (int i = 0; i < line.size(); i++)
+	{
+		if (line[i] == '(' || line[i] == '[' || line[i] == '{')
+			b.push_back(line[i]);
+		else if (line[i] == ')' || line[i] == ']' || line[i] == '}') {
+			if (b.isEmpty())
+				return false;
+			if (b.top() == '(' && line[i] == ')' || b.top() == '[' && line[i] == ']' || b.top() == '{' && line[i] == '}')
+			{
+				b.pop_back();
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+	if (b.isEmpty()) return true;
+	else return false;
+}
 
 int main()
 {
-	//myStack::Stack<int> a;
+	/*myStack::Stack<std::string> a;
 
-	//a.push_back(4);
-	//a.push_back(9);
-	//a.push_back(1);
-	//a.push_back(12);
-	//a.push_back(-5);
-	//// 4 9 1 12 -5
+	a.push_back("students");
+	a.push_back("of");
+	a.push_back("the");
+	a.push_back("group");
+	a.push_back("PV");
+	a.push_back("321");
 
-	//a.showStack();
-	//cout << "------------------------------\n";
-	//// Обработка стека
+	a.showStack();
+	a.pop_back();
+	a.showStack();
 
-	//while (!a.isEmpty())
-	//{
-	//	cout << a.top() << " ";
-	//	a.pop_back();
-	//}
-	//cout << endl;
+	cout << "-----------------------\n";
 
-	cout << "------------------------------\n";
+	while (!a.isEmpty())
+	{
+		if (a.top().size() == 2)
+		{
+			cout << a.top() << " ";
+		}
+		a.pop_back();
+	}
+	cout << endl;
+	a.showStack();*/
 
-	int i;
-	cout << "Enter num: ";
-	cin >> i;
-	myStack::Stack<bool> bin;
+	cout << "[()]{} => " << VaildBrackets("[()]{}") << endl;
+	cout << "][ => " << VaildBrackets("][") << endl;
+	cout << "[({)]} => " << VaildBrackets("[({)]}") << endl;
 }
